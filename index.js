ed05ccc46d5a14cbed99a87378b0a7b7e96e467e
Alex Schmidt <alex.schmidt767@gmail.com>
2022-08-07 14:29:12 -0500

    added arrow key movement and walking animations

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-const inventory = newInventory()
-move(inventory).to(0, 0)
-
-const character = newImage('assets/green-character/static.gif')
-let direction = null,
-    x = 100,
-    y = 250
-move(character).to(x, y)
-
-
-
 // add background
-{
 const ROWS = Math.ceil(window.innerHeight / 100); // each tile is 100x100, round up
 const COLS = Math.ceil(window.innerWidth / 100);  // for incomplete rows and cols
 const horizonRow = Math.ceil(ROWS * .7) // make grass take up 70% of screen
 
 for (let row = 0; row < ROWS; row++) {
     for (let col = 0; col < COLS; col++) {
-        console.log(row, col);
         if (row < horizonRow) {
             move(newImage('/assets/grass.png')).to(col * 100, row * 100);
         } else {
@@ -25,9 +12,33 @@ for (let row = 0; row < ROWS; row++) {
         }
     }
 }
+
+const inventory = newInventory()
+move(inventory).to(0, 0)
+
+const character = newImage('assets/green-character/static.gif')
+
+function handleDirectionChange(direction) {
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
 }
 
-move(newImage('assets/tree.png')).to(200, 450)
+move(character).withArrowKeys(100, 250, handleDirectionChange)
+
+move(newImage('assets/tree.png')).withArrowKeys(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
 move(newImage('assets/crate.png')).to(150, 350)
