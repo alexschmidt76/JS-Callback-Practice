cd0a3aee52eb268f90bace5f3944a2bab608a581
Alex Schmidt <alex.schmidt767@gmail.com>
2022-08-07 23:32:07 -0500

    added boundaries for walking and zIndex changes for when colliding with objects

--- index.js
+++ index.js
@@ -6,18 +6,21 @@ const horizonRow = Math.ceil(ROWS * .7) // make grass take up 70% of screen
 for (let row = 0; row < ROWS; row++) {
     for (let col = 0; col < COLS; col++) {
         if (row < horizonRow) {
-            move(newImage('/assets/grass.png')).to(col * 100, row * 100);
+            move(newImage('/assets/grass.png')).to(col * 100, row * 100, true);
         } else {
-            move(newImage('/assets/sky.png')).to(col * 100, row * 100);        
+            move(newImage('/assets/sky.png')).to(col * 100, row * 100, true);        
         }
     }
 }
 
+// add inventory
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
+// add character
 const character = newImage('assets/green-character/static.gif')
 
+// walking animation for character
 function handleDirectionChange(direction) {
     if(direction === null){
         character.src = 'assets/green-character/static.gif'
@@ -36,9 +39,9 @@ function handleDirectionChange(direction) {
     }
 }
 
+// append images and items to screen
 move(character).withArrowKeys(100, 250, handleDirectionChange)
-
-move(newImage('assets/tree.png')).withArrowKeys(200, 450)
+move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
 move(newImage('assets/crate.png')).to(150, 350)
--- move.js
+++ move.js
@@ -1,20 +1,54 @@
+// array of occupied is that can't be walked on
+let occupied_spaces = []
+
 function move(element) {
     element.style.position = 'fixed'
 
-    function moveToCoordinates(left, bottom) {
+    function moveToCoordinates(left, bottom, background=false) {
         element.style.left = left + 'px'
         element.style.bottom = bottom + 'px'
+
+        if(background) {
+            element.style.zIndex = 0
+        } else {
+            element.style.zIndex = 2
+
+            // set space occupied by object
+
+            // does not return width or height of image
+            /* let width = element.style.width
+            let height = element.style.height */
+            
+            let space = {
+                x1: left,
+                y1: bottom,
+                x2: (left + 100),
+                y2: (bottom + 205) 
+            }
+            occupied_spaces.push(space)
+            console.log(occupied_spaces)
+        }
+        
     }
 
     function moveWithArrowKeys(left, bottom, callback) {
+
         let direction = null;
         let x = left;
         let y = bottom;
 
         element.style.left = x + 'px'
         element.style.bottom = y + 'px'
+        element.style.zIndex = 3
 
         setInterval(function() {
+            // set boundary conditions
+            const LEFT_WALL = 0,
+                  RIGHT_WALL = window.innerWidth,
+                  BOTTOM_WALL = 105, // can't walk on the inventory (5 extra px for shadow)
+                  TOP_WALL = window.innerHeight * .725 // can't walk on the sky
+
+            // move in direction
             if(direction === 'west'){
                 x = x - 1
             }
@@ -27,9 +61,36 @@ function move(element) {
             if(direction === 'south'){
                 y = y - 1
             }
+            
+            // check for boundary conditions
+            if(x < LEFT_WALL) {
+                x += 1
+            }
+            if(x > RIGHT_WALL) {
+                x -= 1
+            }
+            if(y < BOTTOM_WALL) {
+                y += 1
+            }
+            if(y > TOP_WALL) {
+                y -= 1
+            }
 
-            character.style.left = x + 'px'
-            character.style.bottom = y + 'px'
+            // check if space is occupied
+            for(let i = 0; i < occupied_spaces.length; i++) {
+                space = occupied_spaces[i]
+                if((x < space.x2) && (x > space.x1) && (y < space.y2) && (y > space.y1)) {
+                    element.style.zIndex = 1
+                    console.log(element.style.zIndex)
+                    break
+                } else {
+                    element.style.zIndex = 3
+                    console.log(element.style.zIndex)
+                }
+            }
+
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
         }, 1) 
 
         document.addEventListener('keydown', function(e){     
@@ -48,12 +109,12 @@ function move(element) {
                 direction = 'south'
             }
             
-            callback(direction)
+            if(typeof callback === 'function') {callback(direction)}
         })
 
         document.addEventListener('keyup', function(e) {
             direction = null
-            callback(direction)
+            if(typeof callback === 'function') {callback(direction)}
         })
     }
 
